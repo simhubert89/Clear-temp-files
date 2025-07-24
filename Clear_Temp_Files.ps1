97faf464a82df53a5c0bcc9fa08b9ebd685412a4
simhubert89 <sim_hubert@hotmail.com>
2025-07-24 17:56:32 -0400

    Legacy Format of Temp files  Cleanup

---
+++ Clear_Temp_Files.ps1
@@ -0,0 +1,17 @@
+﻿##### Windows CleanUp Script By wuddy #####
+
+$ObjShell = New-Object -ComObject Shell.Application
+$ObjFolder = $ObjShell.NameSpace(0xA)
+$WinTemp = "C:\Windows\Temp\*"
+
+Set-Location "C:\Windows\Temp"
+Remove-Item * -Recurse -Force -ErrorAction SilentlyContinue
+
+Set-Location "C:\Windows\Prefetch"
+Remove-Item * -Recurse -Force -ErrorAction SilentlyContinue
+
+Set-Location "C:\Documents and Settings"
+Remove-Item ".\*\Local Settings\temp\*" -Recurse -Force -ErrorAction SilentlyContinue
+
+Set-Location "C:\Users"
+Remove-Item ".\*\AppData\Local\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue
